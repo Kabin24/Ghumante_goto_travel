0df24d25c913a0728552f1621a11cb668af1e787
kabin shrestha <108089715+Kabin24@users.noreply.github.com>
2023-10-31 22:06:50 +0545

    update navbar section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,23 @@ import './NavbarStyles.css';
 import { MenuItems } from './MenuItems';
 import {Link}  from 'react-router-dom';
 
-class Navbar extends Component{
+class Navbar extends Component {
+    state ={clicked:false};
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked});
+    }
     render(){
         return(
             <nav className='NavbarItems'>
                  <h1 className='navbar-logo'> Ghumate</h1>
-                 <ul className='nav-menu'>
+ 
+                 <div className='menu-icons' onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                  
+                 </div>
+
+
+                 <ul className ={ this.state.clicked ? "nav-menu active" : "nav-menu"}>
                 {MenuItems.map((item,index) =>{
                  return   (
                     <li key={index}>
