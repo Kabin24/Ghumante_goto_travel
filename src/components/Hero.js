8f3b989bf3d027f643e9ff60c77b2c19a0309f7f
kabin shrestha <108089715+Kabin24@users.noreply.github.com>
2023-11-13 20:19:29 +0545

    update hero   component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,20 @@
 import React from "react";
 import  "./HeroStyles.css";
 import hero from './assets/12.jpg';
-function Hero (){
+function Hero (props){
     return(
         <>
-        <div className="hero">
-            <img alt="HeroImg" src={hero}/>
+        <div className={props.cName}>
+            <img alt="HeroImg" src={props.heroImg}/>
+       
+        <div className="hero-text">
+            <h1>{props.title}</h1>
+            <p>{props.text}</p>
+            <a href={props.url} className={props.btnClass}>
+                {props.buttonText}
+              
+            </a>
+            </div>
         </div>
         </>
     )
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,7 +5,16 @@ function Home (){
     return(
         <>
        <Navbar/>
-       <Hero/>
+       <Hero 
+       cName="hero"
+       heroImg="https://images.pexels.com/photos/346885/pexels-photo-346885.jpeg?auto=compress&cs=tinysrgb&w=600"
+       title="Your Journey Your Story "
+       text="Choose Your Favorite Destination"
+       buttonText="Travel Plan"
+       url="/"
+       btnClass="show"
+       />
+
         </>
     );
 
