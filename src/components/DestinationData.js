c389c734f4862904f51b92118f5e07d37c54b5f5
kabin shrestha <108089715+Kabin24@users.noreply.github.com>
2023-11-25 15:36:22 +0545

    added new

---
+++ src/components/DestinationData.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import "./DestinationStyles.css";
+
+class DestinationData extends Component {
+  render() {
+    return (
+      <div>
+        <h1></h1>
+      </div>
+    );
+  }
+}
+
+export default DestinationData;
